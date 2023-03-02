ec7c428a9d95da798a893931fddff415cc4e13f3
ShinBae Kong <osoriz@naver.com>
2023-03-02 14:48:32 -0800

    dotenv

--- standardchat/vite.config.ts
+++ standardchat/vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from 'dotenv';
-dotenv.config(); // .env 파일 로드
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
